ca0cd3dbc30b1c46d27411044df478e3ab4b9a92
mahiranand <mahiranand08@gmail.com>
2024-06-03 16:55:48 +0530

    reder issue fix

--- src/components/CustomerGluComponent.jsx
+++ src/components/CustomerGluComponent.jsx
@@ -24,7 +24,7 @@ const CustomerGluComponent = ({ writeKey, userId }) => {
     return () => {
       document.body.removeChild(script);
     };
-  });
+  }, [writeKey, userId]);
 
   return (
     <></>
