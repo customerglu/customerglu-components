763863cb93cad8d8622e656d0a8a5f4522961802
mahiranand <mahiranand08@gmail.com>
2024-06-03 17:15:19 +0530

    re-render bug fix

--- src/components/CustomerGluComponent.jsx
+++ src/components/CustomerGluComponent.jsx
@@ -1,24 +1,27 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 
 const CustomerGluComponent = ({ writeKey, userId }) => {
-  const scriptLoadedRef = useRef(false);
-
   useEffect(() => {
-    if (!scriptLoadedRef.current) {
+    const initializeCustomerGlu = () => {
+      if (window.CustomerGlu) {
+        new window.CustomerGlu(writeKey, { userId }, {});
+        console.log("CustomerGlu initialized");
+      } else {
+        console.error("CustomerGlu is not available");
+      }
+    };
+
+    const existingScript = document.querySelector(`script[src="https://assets.customerglu.com/scripts/sdk/v5.0/sdk.js"]`);
+
+    if (!existingScript) {
       const script = document.createElement("script");
       script.src = "https://assets.customerglu.com/scripts/sdk/v5.0/sdk.js";
       script.async = true;
 
       script.onload = () => {
         console.log("Script loaded successfully");
-        scriptLoadedRef.current = true;
-        if (window.CustomerGlu) {
-          new window.CustomerGlu(writeKey, { userId }, {});
-          console.log("CustomerGlu initialized");
-        } else {
-          console.error("CustomerGlu is not available");
-        }
+        initializeCustomerGlu();
       };
 
       script.onerror = (error) => {
@@ -29,22 +32,14 @@ const CustomerGluComponent = ({ writeKey, userId }) => {
 
       return () => {
         document.body.removeChild(script);
-        scriptLoadedRef.current = false;
       };
     } else {
-      // If script is already loaded, initialize the SDK directly
-      if (window.CustomerGlu) {
-        new window.CustomerGlu(writeKey, { userId }, {});
-        console.log("CustomerGlu initialized");
-      } else {
-        console.error("CustomerGlu is not available");
-      }
+      // If the script already exists, just initialize the SDK
+      initializeCustomerGlu();
     }
   }, [writeKey, userId]);
 
-  return (
-    <></>
-  );
+  return <div id="embedId"></div>;
 };
 
 CustomerGluComponent.propTypes = {
