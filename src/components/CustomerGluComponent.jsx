0b4d12f2f239b85e2414502fbc24ed90716447a7
mahiranand <mahiranand08@gmail.com>
2024-06-03 18:54:05 +0530

    back to older version

--- src/components/CustomerGluComponent.jsx
+++ src/components/CustomerGluComponent.jsx
@@ -1,45 +1,49 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
 
 const CustomerGluComponent = ({ writeKey, userId }) => {
-  const [scriptLoaded, setScriptLoaded] = useState(false);
-  const [scriptError, setScriptError] = useState(false);
+  const scriptLoadedRef = useRef(false);
 
   useEffect(() => {
-    const initializeCustomerGlu = () => {
+    if (!scriptLoadedRef.current) {
+      const script = document.createElement("script");
+      script.src = "http://127.0.0.1:8080";
+      script.async = true;
+
+      script.onload = () => {
+        console.log("Script loaded successfully");
+        scriptLoadedRef.current = true;
+        if (window.CustomerGlu) {
+          new window.CustomerGlu(writeKey, { userId }, {});
+          console.log("CustomerGlu initialized");
+        } else {
+          console.error("CustomerGlu is not available");
+        }
+      };
+
+      script.onerror = (error) => {
+        console.error("Error loading script:", error);
+      };
+
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+        scriptLoadedRef.current = false;
+      };
+    } else {
+      // If script is already loaded, initialize the SDK directly
       if (window.CustomerGlu) {
         new window.CustomerGlu(writeKey, { userId }, {});
         console.log("CustomerGlu initialized");
       } else {
         console.error("CustomerGlu is not available");
       }
-    };
-
-    if (scriptLoaded) {
-      initializeCustomerGlu();
     }
-  }, [scriptLoaded, writeKey, userId]);
+  }, [writeKey, userId]);
 
   return (
-    <>
-      <Helmet>
-        <script
-          src="http://127.0.0.1:8080/sdk.js"
-          async
-          onLoad={() => {
-            console.log("Script loaded successfully");
-            setScriptLoaded(true);
-          }}
-          onError={(error) => {
-            console.error("Error loading script:", error);
-            setScriptError(true);
-          }}
-        />
-      </Helmet>
-      <div id="embedId"></div>
-      {scriptError && <p>Error loading the CustomerGlu SDK script.</p>}
-    </>
+    <></>
   );
 };
 
