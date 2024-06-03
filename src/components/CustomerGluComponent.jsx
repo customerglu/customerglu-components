3f2daa6a0040000952c1400ee0e6d1fb6427188e
mahiranand <mahiranand08@gmail.com>
2024-06-03 22:27:08 +0530

    eventEmmitter added

--- src/components/CustomerGluComponent.jsx
+++ src/components/CustomerGluComponent.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
-import Lottie from 'react-lottie';
-import animationData from '../assets/defaultNativeBannerLoader.json'
+import Lottie from "react-lottie";
+import animationData from "../assets/defaultNativeBannerLoader.json";
+import { EventEmitter } from "events";
 
 const CustomerGluComponent = ({ writeKey, userId }) => {
+  const eventEmitter = new EventEmitter();
   const scriptLoadedRef = useRef(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!scriptLoadedRef.current) {
-    
       const script = document.createElement("script");
       script.src = "http://192.168.1.4:8080/sdk.js";
+      eventEmitter.on('SDK_STATUS_COMPLETED', () => {
+        console.log('SDK_STATUS_COMPLETED')
+        setIsLoading(false);
+      });
       script.async = true;
 
       script.onload = () => {
@@ -21,7 +26,6 @@ const CustomerGluComponent = ({ writeKey, userId }) => {
         if (window.CustomerGlu) {
           new window.CustomerGlu(writeKey, { userId }, {});
           console.log("CustomerGlu initialized");
-          setIsLoading(false);
         } else {
           console.error("CustomerGlu is not available");
         }
@@ -47,7 +51,7 @@ const CustomerGluComponent = ({ writeKey, userId }) => {
         console.error("CustomerGlu is not available");
       }
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const defaultOptions = {
@@ -55,32 +59,27 @@ const CustomerGluComponent = ({ writeKey, userId }) => {
     autoplay: true,
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   return (
     <>
       {isLoading ? (
         <div>
-            {console.log("Loading...")}
-          <Lottie 
-            options={defaultOptions}
-            height={400}
-            width={400}
-          />
+          {console.log("Loading...")}
+          <Lottie options={defaultOptions} height={400} width={400} />
         </div>
       ) : (
-        <>
-        </>
+        <></>
       )}
     </>
-  )
+  );
 };
 
 CustomerGluComponent.propTypes = {
   writeKey: PropTypes.string.isRequired,
-  userId: PropTypes.string
+  userId: PropTypes.string,
 };
 
 CustomerGluComponent.defaultProps = {
