7ff2458117aca6d63d01997b774772833128b4dd
mahiranand <mahiranand08@gmail.com>
2024-06-11 16:16:38 +0530

    glu token added and unnessary dependency remove

--- src/components/CustomerGluComponent.jsx
+++ src/components/CustomerGluComponent.jsx
@@ -1,15 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
-import Lottie from "react-lottie";
-import animationData from "../assets/simpleLoader.json";
 import { EventEmitter } from "events";
 
-const CustomerGluComponent = ({ writeKey, userId, lottieJson, gluToken }) => {
+const CustomerGluComponent = ({ userId, gluToken, children }) => {
   const eventEmitter = new EventEmitter();
   const scriptLoadedRef = useRef(false);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const writeKey = "";
     if (!scriptLoadedRef.current) {
       const script = document.createElement("script");
       script.src = "https://assets.customerglu.com/scripts/sdk/v5.1/sdk.js";
@@ -19,11 +18,10 @@ const CustomerGluComponent = ({ writeKey, userId, lottieJson, gluToken }) => {
       script.async = true;
 
       script.onload = () => {
-        console.log("Script loaded successfully");
         scriptLoadedRef.current = true;
 
         if (window.CustomerGlu) {
-          new window.CustomerGlu(writeKey, { userId }, {});
+          new window.CustomerGlu(writeKey, { userId, gluToken }, {});
           console.log("CustomerGlu initialized");
         } else {
           console.error("CustomerGlu is not available");
@@ -56,19 +54,10 @@ const CustomerGluComponent = ({ writeKey, userId, lottieJson, gluToken }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: lottieJson,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid meet",
-    },
-  };
-
   return (
     <>
       {isLoading ? (
-        <Lottie options={defaultOptions} height={120} width={120} />
+        {children}
       ) : (
         <></>
       )}
@@ -77,15 +66,13 @@ const CustomerGluComponent = ({ writeKey, userId, lottieJson, gluToken }) => {
 };
 
 CustomerGluComponent.propTypes = {
-  writeKey: PropTypes.string.isRequired,
   userId: PropTypes.string,
-  lottieJson: PropTypes.object,
-  gluToken: PropTypes.string
+  gluToken: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 CustomerGluComponent.defaultProps = {
   userId: "",
-  lottieJson: animationData,
 };
 
 export default CustomerGluComponent;
